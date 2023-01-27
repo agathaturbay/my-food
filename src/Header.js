4b5acd40cc42e91ce52045f3d258880fd2f3e58b
Agatha Garcia <desenvolvedor02@senografia.com.br>
2023-01-27 09:44:36 -0300

    feat/Basic header structure

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+function Header (){
+    return(
+        <header className="Header-hi">
+            <div class="Title">MyFood</div>
+        </header>
+    )
+}
+
+export default Header;
