5a13f69da92ec9b2be92ad9716921e6b88b6845e
Agatha Garcia <desenvolvedor02@senografia.com.br>
2023-02-01 17:03:16 -0300

    style/Adding style sheet

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,4 +1,8 @@
 import './Header.scss';
+import '../style.scss'
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiOutlineGithub } from 'react-icons/ai';
+import { AiOutlineInstagram } from 'react-icons/ai';
 import '../App';
 import logo from '../assets/Logo.png'
 import { NavLink } from 'react-router-dom';
@@ -7,16 +11,23 @@ import { NavLink } from 'react-router-dom';
 function Header (){
     return(
         <div className="Header_bg">
-            <NavLink to="/">
-                <img src={logo} alt="logo"/>
-            </NavLink>
-            <div className="Title">
+            <div className='Nav-bts'>
                 <NavLink className="Button" to="/">
                     Home
                 </NavLink>
                 <NavLink className="Button" to="/recipes">
                     Recipes
                 </NavLink>
+            </div>
+            <div className="Home_logo">
+                <NavLink to="/">
+                    <img src={logo} alt="logo"/>
+                </NavLink>
+            </div>
+            <div className='Social-media'>
+                <AiFillLinkedin />
+                <AiOutlineGithub />
+                <AiOutlineInstagram />
             </div>
       </div>
     )
