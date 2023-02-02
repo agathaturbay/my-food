ee6e1eb895b1c39551b973fda64e588915ebb614
Agatha Garcia <desenvolvedor02@senografia.com.br>
2023-02-02 08:39:42 -0300

    feat/Adding social media buttons to header

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -5,7 +5,7 @@ import { AiOutlineGithub } from 'react-icons/ai';
 import { AiOutlineInstagram } from 'react-icons/ai';
 import '../App';
 import logo from '../assets/Logo.png'
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 
 function Header (){
@@ -25,9 +25,17 @@ function Header (){
                 </NavLink>
             </div>
             <div className='Social-media'>
-                <AiFillLinkedin />
-                <AiOutlineGithub />
-                <AiOutlineInstagram />
+                <div className='Align-middle'>
+                    <Link to={{ pathname: "https://www.instagram.com/garciaa_tata/" }} target="_blank" className='Btn'>
+                        <AiOutlineInstagram />
+                    </Link>
+                    <Link to={{ pathname: "https://www.linkedin.com/in/agatha-garcia-1a650b213/" }} target="_blank" className='Btn'>
+                        <AiFillLinkedin />
+                    </Link>
+                    <Link to={{ pathname: "https://github.com/agathaturbay" }} target="_blank" className='Btn'>
+                        <AiOutlineGithub />
+                    </Link>
+                </div>
             </div>
       </div>
     )
