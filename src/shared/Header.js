8cf4ae81f3c6e027c9e0142819a5ae8213243f2c
Agatha Garcia <desenvolvedor02@senografia.com.br>
2023-02-02 09:13:18 -0300

    style/Fixing header css

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -10,29 +10,31 @@ import { NavLink, Link } from 'react-router-dom';
 
 function Header (){
     return(
-        <div className="Header_bg">
+        <div className="Header_bg ">
             <div className='Nav-bts'>
-                <NavLink className="Button" to="/">
-                    Home
-                </NavLink>
-                <NavLink className="Button" to="/recipes">
-                    Recipes
-                </NavLink>
+                <div className='Align-middle'>
+                    <NavLink className="Page-button" to="/">
+                        Home
+                    </NavLink>
+                    <NavLink className="Page-button" to="/recipes">
+                        Recipes
+                    </NavLink>
+                </div>
             </div>
             <div className="Home_logo">
                 <NavLink to="/">
-                    <img src={logo} alt="logo"/>
+                    <img className="Logo-img Align-middle" src={logo} alt="logo"/>
                 </NavLink>
             </div>
             <div className='Social-media'>
                 <div className='Align-middle'>
-                    <Link to={{ pathname: "https://www.instagram.com/garciaa_tata/" }} target="_blank" className='Btn'>
+                    <Link to={{ pathname: "https://www.instagram.com/garciaa_tata/" }} target="_blank" className='Social-button'>
                         <AiOutlineInstagram />
                     </Link>
-                    <Link to={{ pathname: "https://www.linkedin.com/in/agatha-garcia-1a650b213/" }} target="_blank" className='Btn'>
+                    <Link to={{ pathname: "https://www.linkedin.com/in/agatha-garcia-1a650b213/" }} target="_blank" className='Social-button'>
                         <AiFillLinkedin />
                     </Link>
-                    <Link to={{ pathname: "https://github.com/agathaturbay" }} target="_blank" className='Btn'>
+                    <Link to={{ pathname: "https://github.com/agathaturbay" }} target="_blank" className='Social-button'>
                         <AiOutlineGithub />
                     </Link>
                 </div>
